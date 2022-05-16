1702409a07a75553b87bbacdb937dcbd5b2572b4
Aleordoh <aleordoh@gmail.com>
2022-05-16 14:36:51 -0300

    iniciando

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require ("path");
+
+app.listen(3000, () => console.log(`servidor corriendo en el puerto ${PORT}`));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get('/login.html', (req,res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
