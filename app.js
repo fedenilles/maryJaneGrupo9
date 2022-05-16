bed7e8125681a73513dcc9833e17d6b04de273fd
joackwolf <joacow88@gmail.com>
2022-05-15 22:26:27 -0300

    login html y css  added + appExpresHomeFede

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+
+app.get('/login.html', (req,res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
