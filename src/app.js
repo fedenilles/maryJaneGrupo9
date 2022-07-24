d6c526046143ea0fceacfc4a6c41ca4a28aacaf5
aleordoh <aleordoh@gmail.com>
2022-07-23 22:40:18 -0300

    Arreglado controller y vista products detail y edit

--- src/app.js
+++ src/app.js
@@ -45,13 +45,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-
-
 app.use("/",mainRoutes)
 app.use("/products",productsRoutes)
 /* app.use(unless("/users",userLoggedMiddleware),usersRoutes) */
 app.use("/users",usersRoutes)
-
 
 
 app.listen(3000, () => console.log(`servidor corriendo en el puerto ${PORT}`));
